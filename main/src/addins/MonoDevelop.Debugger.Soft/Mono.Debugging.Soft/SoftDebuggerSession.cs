787bed1589b496d2fd66f6bacc974b88db281838
Levi Bard <levi@unity3d.com>
2010-11-03 17:23:44 +0100

    [SDB] Disconnect from VM on detach.
    * Mono.Debugging.Soft/SoftDebuggerSession.cs: 
      Disconnect from VM on detach.
    
    License: MIT/X11

--- main/src/addins/MonoDevelop.Debugger.Soft/Mono.Debugging.Soft/SoftDebuggerSession.cs
+++ main/src/addins/MonoDevelop.Debugger.Soft/Mono.Debugging.Soft/SoftDebuggerSession.cs
@@ -440,7 +440,8 @@ namespace Mono.Debugging.Soft
 		protected override void OnDetach ()
 		{
 			EndLaunch ();
-			vm = null;
+			vm.Disconnect ();
+			vm.Dispose ();
 		}
 
 		protected override void OnExit ()
